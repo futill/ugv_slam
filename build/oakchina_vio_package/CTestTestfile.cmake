745056d6cea6237ea6b65e64604a673eb88ddf75
“futill” <358181022@qq.com>
2025-06-29 21:25:48 +0800

    11

--- build/oakchina_vio_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/orangepi/uvg_car/src/oakchina_vio_package_ros2
-# Build directory: /home/orangepi/uvg_car/build/oakchina_vio_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/wit_ros2_imu/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/orangepi/uvg_car/install/wit_ros2_imu'
